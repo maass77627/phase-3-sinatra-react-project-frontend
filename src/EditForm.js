95dc2018705cc5031018a01b3438944af10b1007
Kelly Maass <kmaass77627@gmail.com>
2026-01-27 12:30:29 -0600

    edit form

--- src/EditForm.js
+++ src/EditForm.js
@@ -16,16 +16,30 @@ function EditForm({recipe}) {
 
 
 
+        function handleChange(e) {
+            let name = e.target.name
+            let value = e.target.value
+            setFormData({...formData, [name]: value})
+
+        }
+
+        function handleSubmit(e) {
+            e.preventDefault()
+            fetch()
+        }
+
 
 
     return (
 
         <div className="edit-form">
             <form>
-                <input type="text" value={formData.name}></input>
-                <input type="text" value={formData.ingredients}></input>
-                <input type="text" value={formData.directions}></input>
-                <input type="submit" ></input>
+                <input onChange={(e) => handleChange(e)} type="text" name="name" value={formData.name}></input>
+                <input onChange={(e) => handleChange(e)} type="text" name="ingredients" value={formData.ingredients}></input>
+                <input onChange={(e) => handleChange(e)} type="text" name="directions" value={formData.directions}></input>
+                <input onChange={(e) => handleChange(e)} type="text" name="directions" value={formData.directions}></input>
+                <input onChange={(e) => handleChange(e)} type="text" name="directions" value={formData.directions}></input>  
+                <input type="submit" value="submit" ></input>
 
             </form>
 
