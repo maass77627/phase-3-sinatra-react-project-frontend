603e13ce99fa8f77a9410dcc79fd144365c2fb26
Kelly Maass <kmaass77627@gmail.com>
2026-01-29 12:00:38 -0600

    done

--- src/RecipeRotator.js
+++ src/RecipeRotator.js
@@ -2,18 +2,19 @@ import React from "react";
 // import { useState } from "react";
 
 function RecipeRotator({rec}) {
-    
-   
+    console.log(new Date())
+   console.log(Date.now())
+   console.log(new Date().toLocaleDateString("en-US"))
 
     return (
 
-        <div className="reciperotator">
-            {/* {time} */}
+        <div className="recipe-rotator">
+            
             
             
             <i style={{color: "green"}} className="fa-regular fa-face-laugh"></i>
-            <i style={{color: "red"}}className="fa-regular fa-face-frown"></i>
-            <i style={{color: "yellow"}}className="fa-regular fa-face-meh"></i>
+            <i style={{color: "red"}} className="fa-regular fa-face-frown"></i>
+            <i style={{color: "yellow"}} className="fa-regular fa-face-meh"></i>
 
             
         </div>
